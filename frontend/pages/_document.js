4fdc8b9483664f337f4181d2684b5bfe931d0ebb
Balaji K <balajibala76394@gmail.com>
2023-04-02 12:19:25 +0530

    header init

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -7,6 +8,11 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        
+<Script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" strategy="beforeInteractive" />
+<Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" strategy="beforeInteractive" />
+<Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js" strategy="beforeInteractive" />
+
       </body>
     </Html>
   )
